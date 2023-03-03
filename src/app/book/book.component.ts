6c863a203a39042add5f085ca1dba257e89f3c3f
vishek-patel <vishek.12001921@lpu.in>
2023-03-03 14:19:35 +0530

    day 1 of angular

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+
+  temp:string = ""
+  placeHolder:string = "Enter a todo..."
+  isDisabled:boolean = false
+  toShow:boolean = true
+  inputValue:string = ""
+  toDisplay:boolean = true
+  names:string[] = []
+  source:string = ""
+  alternate:string = ""
+
+  handleClick(event:any) {
+    console.log(event);
+    this.toShow = !this.toShow;
+    // this.isDisabled = !this.isDisabled;
+  }
+
+  handleImage(){
+    this.source = "https://www.freecodecamp.org/news/content/images/size/w1000/2020/10/461803-angular-JavaScript-HTML-1.jpg"
+    this.alternate = "Angular Logo image"
+  }
+
+  handleInput(event:any){
+    if(event.key === "Enter"){
+      this.names.unshift(event.target.value)
+      this.temp = ""
+    }
+    else {
+      this.temp = event.target.value
+    }
+    // this.temp = event.target.value
+  }
+  addTodo(){
+    this.names.unshift(this.temp)
+  }
+  deleteTodo(){
+    this.names.pop()
+  }
+  handleDisplay(){
+    // console.log(event)
+    // console.log(this.inputValue)
+    this.names = this.inputValue.split(" ")
+
+    console.log(this.names)
+
+
+    // this.toDisplay = !this.toDisplay
+    // this.names=(this.inputValue.split(" "))
+  }
+  deleteItem(index: number){
+    this.names.splice(index, 1);
+  }
+
+  day:string = "1"
+  switch(day:string){
+    this.day = "Monday"
+    this.day = "Sunday"
+
+  }
+
+}
